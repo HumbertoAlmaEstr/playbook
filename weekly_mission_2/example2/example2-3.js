91cc4c8bf4ad42c9c9561c48fbf0ce03d8314ca9
HumbertoAlmaEstr <humbertoestrada0210@gmail.com>
2022-04-25 20:56:55 -0500

    Upload example and exercises

---
+++ weekly_mission_2/example2/example2-3.js
@@ -0,0 +1,11 @@
+class Student {
+    // El constructor nos permite instanciar un objeto y asignarle atributos, this nos ayuda a realizar esto.
+    constructor(name, age, subjects){
+        this.name = name
+        this.age = age
+        this.subjects = subjects
+    }
+}
+const carloStudent = new Student("Carlo", 12, ["NodeJs", "Python"])
+console.log("Ejemplo 3: Instanciar un objeto con atributos")
+console.log(carloStudent)
---
+++ weekly_mission_2/example2/example2-9.js
@@ -0,0 +1,27 @@
+class Developer {
+
+    constructor(name, mainLang, stack){
+
+        this.name =  name
+        this.mainLang = mainLang
+        this.stack = stack
+}
+  
+get getName() {
+
+    return this.name
+    }
+}
+  
+console.log("Ejemplo  9: Herencia entre dos clases")
+const carloDev = new Developer("Humberto", "js", ["elixir", "groovy", "lisp"])
+console.log(carloDev)
+  
+  // Se usa la palabra extends para indicar que heredarás las propiedades de la clase Padre (Developer) en la clase definida.
+  // Podemos definir una clase vacía y rehusar todos los componentes de la clase padre
+class LaunchXStudent extends Developer{
+}
+  
+const humbertoLaunchXDev = new LaunchXStudent("Humberto", "js", ["elixir", "groovy", "lisp"])
+console.log(humbertoLaunchXDev)
+console.log(humbertoLaunchXDev.getName) // getter de la clase padre rehusada en la clase hija
